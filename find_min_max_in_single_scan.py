58d46e36750270f6f8de006edd83fddfc8bef092
Prashant Kolhar <60572402+Kolhar-Prashant@users.noreply.github.com>
2020-09-22 12:57:20 +0530

    Update find_min_max_in_single_scan.py

--- find_min_max_in_single_scan.py
+++ find_min_max_in_single_scan.py
@@ -3,11 +3,9 @@ x=[4586,7,8,10,12,5,17,19,4]
 
 min=x[0]
 max=x[0]
-c=0
-for e in x:
-    c+=1
+for e in x:    
     if e < min:
         min = e
     if e > max:
         max=e
-print(min,max,c)
+print(min,max)
