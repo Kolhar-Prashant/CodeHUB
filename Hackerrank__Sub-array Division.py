b8c54ed157cfc31f0f466e8fa87d7c6c6d86a4d8
Prashant Kolhar <60572402+Kolhar-Prashant@users.noreply.github.com>
2020-10-17 11:11:12 +0530

    Add files via upload

---
+++ Hackerrank__Sub-array Division.py
@@ -0,0 +1,23 @@
+def birthday(s, d, m):
+    L = s
+    tar = d
+    if len(L) == 1:
+        Len = 2
+    else:
+        Len = len(L)
+    indx = 0
+    cnt = 0
+    while indx != Len-1:
+        min_indx = indx
+        sum = 0
+        try:
+            for _ in range(m):
+                print(L[min_indx])
+                sum += L[min_indx]
+                min_indx += 1
+        except:
+            return cnt        
+        if sum == tar:
+            cnt += 1
+        indx += 1
+    return cnt
