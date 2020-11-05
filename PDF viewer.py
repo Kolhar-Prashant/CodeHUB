44767cdca825c4a2afb3acbacf600ba4ad848f63
Prashant Kolhar <60572402+Kolhar-Prashant@users.noreply.github.com>
2020-11-05 17:03:52 +0530

    Update PDF viewer.py

--- PDF viewer.py
+++ PDF viewer.py
@@ -1,19 +1,17 @@
-h = [1, 3, 1, 3, 1, 4, 1, 3 ,2 ,5, 5, 5, 5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,7]
-word = "zaba"
-s = "the quick brown fox jumps over the lazy dog"
-s = s.replace(" ","")
-s = sorted(set(s))
+def designerPdfViewer(h, word):
+    s = "the quick brown fox jumps over the lazy dog"
+    s = s.replace(" ","")
+    s = sorted(set(s))
 
-indx = 0
-Dict = {}
-for num in h:
-    Dict[s[indx]] = num
-    indx+=1
+    indx = 0
+    Dict = {}
+    for num in h:
+        Dict[s[indx]] = num
+        indx+=1
 
-maxx = 0
-for alpha in word:
-    height = Dict[alpha]
-    if height > maxx:
-        maxx = height
-
-print(maxx * len(word))
+    maxx = 0
+    for alpha in word:
+        height = Dict[alpha]
+        if height > maxx:
+            maxx = height
+    return maxx * len(word)
